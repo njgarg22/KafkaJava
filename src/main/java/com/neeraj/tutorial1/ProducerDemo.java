a2462cd0fcbc32460a5c53a898ed7fdc08e76990
Neeraj Garg <njgarg22@gmail.com>
2021-08-14 23:08:38 +0530

    Add a simple producer

--- src/main/java/com/neeraj/tutorial1/ProducerDemo.java
+++ src/main/java/com/neeraj/tutorial1/ProducerDemo.java
@@ -1,8 +1,42 @@
 package com.neeraj.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
+        String bootstrapServers = "localhost:9092";
 
+        // Step1: Create producer properties
+        Properties properties = new Properties();
 
+        /* Old way of setting properties
+        properties.setProperty("bootstrap.servers", bootstrapServers);
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+         */
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Step2: Create producer
+        KafkaProducer<String, String > producer = new KafkaProducer<>(properties);
+
+        // Step3.0: Create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("java_topic", "hello world");
+
+        // Step3.1: Send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
     }
 }
